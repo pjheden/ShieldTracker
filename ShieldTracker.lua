591bb539f173d17dc5f5b8e35d41ab56b0eb2688
pjheden <julianheden@gmail.com>
2019-11-02 23:33:06 +0100

    Add steps

--- ShieldTracker.lua
+++ ShieldTracker.lua
@@ -1,4 +1,25 @@
 function Init()
   print("ShieldTracker: init");
   local f = CreateFrame("Frame");
+
+  TOP LEVEL:
+  NECESSARY
+  * Register to correct events,
+  * Store or get data of shield spell and HP,
+  * Visualize health of shield,
+
+  EXTRA
+  * Play sound on break
+  * Shield health on other players
+  * (Store what players have what talents)
+
+  Step 0: Register events to listen to buffs on player
+  Step 1: On buff event check if the spell is a shield spell
+  Step 2: Get the health of the shield spell and set shield variabes
+  Step 3: Register so you listen to damage taken
+  Step 4: On damage taken, update shield variables
+  Step 5: Visualize health on shield based on shield variables (TEXT / number on screen / bar on screen)
+
+
+  Step 6: Play sound when shield break
 end
