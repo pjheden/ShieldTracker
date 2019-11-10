7c3b221c9f1e7bee35f099527d85b1e82d95bdc2
pjheden <julianheden@gmail.com>
2019-11-10 01:34:19 +0100

    Make functional with unfiltered combat log

--- ShieldTracker.lua
+++ ShieldTracker.lua
@@ -5,15 +5,10 @@ function Init()
     ["Mana Shield"] = 570,
     ["Ice Barrier"] = 826
   }
-  currentShield = {
-    ["HP"] = 0,
-    ["Name"] = ""
-  }
 
+  currentShields = {}
 
   local f = CreateFrame("Frame");
-  -- Step 0: Register events to listen to buffs on player
-  f:RegisterEvent("UNIT_AURA")
   -- Step 3: Register so you listen to damage taken
   f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
   -- Step 1: On buff event check if the spell is a shield spell
@@ -31,70 +26,104 @@ function Init()
   -- * (Store what players have what talents)
 end
 
-
 function OnEvent(self, event, ...)
-  if event == "UNIT_AURA" then
-    OnUnitAura(...)
-  elseif event == "COMBAT_LOG_EVENT_UNFILTERED" then
+  if event == "COMBAT_LOG_EVENT_UNFILTERED" then
     OnCombatUpdate()
   end
 end
 
-function OnUnitAura(...)
-  local unitTarget = ...
-  if unitTarget ~= "player" then
-    return
-  end
-  print("unitTarget " .. unitTarget)
-
-  -- Iterate over player buffs to look for shield
-  for i=1,50 do
-    -- TODO: bug where shield hp refreshes whern another aura is added
-    local name, _, _, _, duration, expirationTime, source, _, _, spellID = UnitAura("player", i)
-    if name == nil then
-      print("No more auras, name == nil")
-      break
+local function ParseLogMessage(timestamp, event, hideCaster, sourceGUID,
+   sourceName, sourceFlags, sourceRaidFlags, recipientGUID, recipientName,
+   recipientFlags, recipientRaidFlags, ...)
+  -- TODO: get current player name
+  if recipientName == "Icebag" or recipientName == "Mardur" then
+    print(event)
+    if event == "SPELL_ABSORBED" then
+      OnSpellAbsorbed(...)
+    elseif event == "SPELL_AURA_APPLIED" then
+      OnAuraApplied(...)
+    elseif event == "SPELL_AURA_REFRESH" then
+      OnAuraRefreshed(...)
+    elseif event == "SPELL_AURA_REMOVED" then
+      OnAuraRemoved(...)
     end
-    -- TODO: add other spells and maybe use spellID instead
-    local currentShieldHealth = shieldHealth[name]
-    if currentShieldHealth ~= nil then
-      -- Step 2: Get the health of the shield spell and set shield variables
-      print("Power shield found on player, setting shield variables!")
-      currentShield.HP = currentShieldHealth
-      currentShield.Name = name
+  end
+end
+
+function OnSpellAbsorbed(...)
+  -- firstARg is some unknown variable, but it seems that the return arugments
+  -- are different if it is 0 compared to non zero. Has something to do with
+  -- if it is melee dmg or caster dmg.
+  local firstArg = ...
+  if firstArg == 0 then
+    local _, _, _, _, _, _, _, shieldID, shieldName, _, dmg = ...
+    UpdateShieldHealth(shieldName, dmg)
+  else
+    local _, _, _, _, shieldID, shieldName, _, dmg = ...
+    UpdateShieldHealth(shieldName, dmg)
+  end
+end
+
+function UpdateShieldHealth(name, dmg)
+  for i = 1, #currentShields + 1 do
+    if name == currentShields[i].Name then
+      currentShields[i].HP = currentShields[i].HP - dmg
+      print("============")
+      print("Current Shield Name: " .. currentShields[i].Name)
+      print("Current Shield HP: " .. currentShields[i].HP)
+      print("============")
       break
     end
   end
 end
 
--- TODO rename this
-local function ParseLogMessage(timestamp, event, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, recipientGUID, recipientName, recipientFlags, recipientRaidFlags, ...)
-  -- TODO: get current player name
-  if (recipientName == "Icebag" or recipientName == "Mardur") and event == "SPELL_ABSORBED" then
-    -- TODO: find better method to do this
-    local firstArg = ...
-    if firstArg == 0 then
-      local _, _, _, _, _, _, _, shieldID, shieldName, _, dmg = ...
-      print("Current Shield HP BEFORE: " .. currentShield.HP)
-      currentShield.HP = currentShield.HP - dmg
-      print("Current Shield HP AFTER: " .. currentShield.HP)
-    else
-      local _, _, _, _, shieldID, shieldName, _, dmg = ...
-      print("Current Shield HP BEFORE: " .. currentShield.HP)
-      -- -- Step 4: On damage taken, update shield variables
-      currentShield.HP = currentShield.HP - dmg
-      -- Step 5: Visualize health on shield based on shield variables (TEXT / number on screen / bar on screen)
-      print("Current Shield HP AFTER: " .. currentShield.HP)
-    end
+-- NEW SHIELD ON TARGET
+function OnAuraApplied(...)
+  -- Kolla att det är en aura vi bryr oss om
+  -- Add to currentShields list
+  local _, shieldName = ...
+  print("OnAuraApplied: " .. shieldName)
+  local currentShieldHealth = shieldHealth[shieldName]
+  if currentShieldHealth ~= nil then
+    curShield = {
+      ["HP"] = currentShieldHealth,
+      ["Name"] = shieldName
+    }
+    table.insert(currentShields, curShield)
+  end
+end
 
-  elseif (recipientName == "Icebag" or recipientName == "Mardur") and event == "SPELL_AURA_REMOVED" then
-    local _, spellName = ...
-    if spellName == currentShield.Name then
+-- CAST SAME SHIELD AGAIN
+function OnAuraRefreshed(...)
+  local _, shieldName = ...
+  print("OnAuraRefreshed: " .. shieldName)
+  -- Get health from DB
+  local currentShieldHealth = shieldHealth[shieldName]
+  if currentShieldHealth == nil then return end
+  -- Check if we have it in our list of shields
+  for i = 1, #currentShields + 1 do
+    if shieldName == currentShields[i].Name then
+      -- Update shield data
+      currentShields[i].HP = currentShieldHealth
+    end
+  end
+
+end
+
+-- REMOVED, EXPIRED, OUT OF HEALTH
+function OnAuraRemoved(...)
+  local _, shieldName = ...
+  print("OnAuraRemoved: "..shieldName)
+  -- Check if we have it in our list of shields
+  for i = 1, #currentShields + 1 do
+    if shieldName == currentShields[i].Name then
+      table.remove(currentShields, i)
       print("MACKE SHIELDA MIG FÖR FAN")
       -- TODO: Play sound if right aura?
+      break
     end
-
   end
+
 end
 
 function OnCombatUpdate()
